86bb473ba104cd03a69808438aaafaee602b32b0
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2021-03-16 14:36:27 -0400

    use reach dialog to show login/registration popup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import '@reach/dialog/styles.css'
+import React, {useState} from 'react'
 import {Logo} from 'components/Logo'
+import Dialog from '@reach/dialog'
 
 function App() {
+  const [openModal, setOpenModal] = useState('none')
   return (
     <div>
       <Logo width={80} height={80} />
       <h1>Bookshelf</h1>
       <div>
-        <button onClick={() => alert('login clicked')}>Login</button>
+        <button onClick={() => setOpenModal('login')}>Login</button>
       </div>
       <div>
-        <button onClick={() => alert('Register clicked')}>Register</button>
+        <button onClick={() => setOpenModal('register')}>Register</button>
       </div>
+      <Dialog aria-label="Login form" isOpen={openModal === 'login'}>
+        <button onClick={() => setOpenModal('none')}>close</button>
+        <h2>Login</h2>
+      </Dialog>
+      <Dialog aria-label="Register form" isOpen={openModal === 'register'}>
+        <button onClick={() => setOpenModal('none')}>close</button>
+        <h2>Register</h2>
+      </Dialog>
     </div>
   )
 }
