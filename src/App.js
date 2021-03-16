52afce0bf79e1ec6fcb92bdd740e7e0cec6a63b1
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2021-03-16 14:22:40 -0400

    add app basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import {Logo} from 'components/Logo'
 
 function App() {
-  return <div>Hello App</div>
+  return (
+    <div>
+      <Logo width={80} height={80} />
+      <h1>Bookshelf</h1>
+      <div>
+        <button onClick={() => alert('login clicked')}>Login</button>
+      </div>
+      <div>
+        <button onClick={() => alert('Register clicked')}>Register</button>
+      </div>
+    </div>
+  )
 }
 
 export default App
