cc8b04b9241840c84563953e6b0eeeed38fd364d
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2021-03-17 11:03:36 -0400

    add more lib components

--- src/components/lib.js
+++ src/components/lib.js
@@ -1,13 +1,31 @@
-import styled from '@emotion/styled'
+import styled from '@emotion/styled/macro'
+import {Dialog as ReachDialog} from '@reach/dialog'
+import {keyframes} from '@emotion/react'
+
+import {FaSpinner} from 'react-icons/fa'
+import * as colors from 'styles/colors'
+import * as mq from 'styles/media-queries'
+
+const spin = keyframes({
+  '0%': {transform: 'rotate(0deg)'},
+  '100%': {transform: 'rotate(360deg)'},
+})
+
+const Spinner = styled(FaSpinner)({
+  animation: `${spin} 1s linear infinite`,
+})
+Spinner.defaultProps = {
+  'aria-label': 'loading',
+}
 
 const buttonVariants = {
   primary: {
-    backgroundColor: '#3f51b5',
-    color: 'white',
+    background: colors.indigo,
+    color: colors.base,
   },
   secondary: {
-    backgroundColor: '#f1f2f7',
-    color: '#434449',
+    background: colors.gray,
+    color: colors.text,
   },
 }
 
@@ -21,4 +39,43 @@ const Button = styled.button(
   ({variant = 'primary'}) => buttonVariants[variant],
 )
 
-export {Button}
+const Input = styled.input({
+  borderRadius: '3px',
+  border: '1px solid #f1f1f4',
+  background: '#f1f2f7',
+  padding: '8px 12px',
+})
+
+const CircleButton = styled.button({
+  borderRadius: '30px',
+  padding: '0',
+  width: '40px',
+  height: '40px',
+  lineHeight: '1',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  background: colors.base,
+  color: colors.text,
+  border: `1px solid ${colors.gray10}`,
+  cursor: 'pointer',
+})
+
+const Dialog = styled(ReachDialog)({
+  maxWidth: '450px',
+  borderRadius: '3px',
+  paddingBottom: '3.5em',
+  boxShadow: '0 10px 30px -5px rgba(0, 0, 0, 0.2)',
+  margin: '20vh auto',
+  [mq.small]: {
+    width: '100%',
+    margin: '10vh auto',
+  },
+})
+
+const FormGroup = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+})
+
+export {Button, CircleButton, Dialog, FormGroup, Input, Spinner}
